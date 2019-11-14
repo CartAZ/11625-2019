68288b8bbbff1bf5ea54e890b8648c1ad388d3a2
Cadre1 <55156855+Cadre1@users.noreply.github.com>
2019-11-13 19:24:14 -0800

    Servo motor for grabber

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/XDrive1Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/XDrive1Test.java
@@ -38,6 +38,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 import java.math.*;
 
 /**
@@ -53,11 +54,16 @@ public class XDrive1Test extends OpMode {
 	DcMotor motorFrontLeft;
 	DcMotor motorBackRight;
 	DcMotor motorBackLeft;
+	Servo servoGrabber;
+
 
 	boolean bDebugFR = false;
 	boolean bDebugFL = false;
 	boolean bDebugBR = false;
 	boolean bDebugBL = false;
+	boolean bDebugGrabber = false;
+
+	float grabRotation = 0;
 
 	/**
 	 * Constructor
@@ -106,11 +112,17 @@ public class XDrive1Test extends OpMode {
 			bDebugBR = true;
 		}
 		try{
-			motorBackLeft = hardwareMap.dcMotor.get("bl");;
+			motorBackLeft = hardwareMap.dcMotor.get("bl");
 			motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
 		}
 		catch (IllegalArgumentException iax) {
 			bDebugBL = true;
+		}
+		try{
+			servoGrabber = hardwareMap.servo.get("grab");
+		}
+		catch(IllegalArgumentException iax){
+			bDebugGrabber = true;
 		}
 	}
 
@@ -124,25 +136,31 @@ public class XDrive1Test extends OpMode {
 
 		// tank drive
 		// note that if y equal -1 then joystick is pushed all of the way forward.
-		float x = gamepad1.left_stick_x;
-		float y = -gamepad1.left_stick_y;
-		float r = -gamepad1.right_stick_x;
+		float x1 = gamepad1.left_stick_x;
+		float y1 = -gamepad1.left_stick_y;
+		float r1 = gamepad1.right_stick_x;
+		float y2 = -gamepad2.left_stick_y;
 
 		// clip the right/left values so that the values never exceed +/- 1
-		x = Range.clip(x, -1, 1);
-		y = Range.clip(y, -1, 1);
-		r = Range.clip(r, -1, 1);
+		x1 = Range.clip(x1, -1, 1);
+		y1 = Range.clip(y1, -1, 1);
+		r1 = Range.clip(r1, -1, 1);
+		y2 = Range.clip(y2, -1, 1);
 
 		// scale the joystick value to make it easier to control
 		// the robot more precisely at slower speeds.
-		x = (float)scaleInput(x);
-		y = (float)scaleInput(y);
-		r = (float)scaleInput(r);
+		x1 = (float)scaleInput(x1);
+		y1 = (float)scaleInput(y1);
+		r1 = (float)scaleInput(r1);
+		y2 = (float)scaleInput(y2);
 
-		float fr = (y-x)-r;
-		float br = (y+x)-r;
-		float fl = (y+x+r);
-		float bl = (y-x)+r;
+		float fr = (y1-x1)-r1;
+		float br = (y1+x1)-r1;
+		float fl = (y1+x1+r1);
+		float bl = (y1-x1)+r1;
+
+		float grabRotationIncrement = y2/10;
+		grabRotation += grabRotationIncrement;
 
 		if(Math.abs(fr) > 1 && Math.abs(fr) >= Math.abs(br) && Math.abs(fr) >= Math.abs(fl) && Math.abs(fr) >= Math.abs(bl)){
 			br = br / Math.abs(fr);
@@ -169,12 +187,20 @@ public class XDrive1Test extends OpMode {
 			bl = bl / Math.abs(bl);
 		}
 
+		if(grabRotation > 1){
+			grabRotation = 1;
+		}
+		else if(grabRotation < 0){
+			grabRotation = 0;
+		}
+
 		// write the values to the motors - for now, front and back motors on each side are set the same
 		if (!bDebugFR || !bDebugBR || !bDebugFL || !bDebugBL) {
 			motorFrontRight.setPower(fr);
 			motorBackRight.setPower(br);
 			motorFrontLeft.setPower(fl);
 			motorBackLeft.setPower(bl);
+			servoGrabber.setPosition(grabRotation);
        /*
        if(r != 0) {
           x = 0;
@@ -224,6 +250,14 @@ public class XDrive1Test extends OpMode {
 			telemetry.addData("back left pwr", String.format("not working"));
 		}
 		telemetry.addData("gamepad1", gamepad1);
+
+		if(!bDebugGrabber){
+			telemetry.addData("grabber rotation", String.format("%.2f",grabRotation));
+		}
+		else{
+			telemetry.addData("grabber rotation", String.format("not working"));
+		}
+		telemetry.addData("gamepad2", gamepad2);
 	}
 
 	/*
